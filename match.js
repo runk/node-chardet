7f82a455b64f57b61e7d91adbec6e83535fce3e0
Dmitry Shirokov <deadrunk@gmail.com>
2013-05-07 15:01:08 +1000

    minor changes

--- match.js
+++ match.js
@@ -1,7 +1,5 @@
 
 module.exports = function(det, rec, confidence, name, lang) {
-    // console.log(det, rec, confidence);
-    // this.res = 1;
     this.confidence = confidence;
     this.name       = name || rec.name(det);
-}
+};
