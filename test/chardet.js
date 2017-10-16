802964244f1ebae1962aff3ec4c626f5dfa12450
Dmitry Shirokov <deadrunk@gmail.com>
2017-10-16 11:42:49 +1100

    add sample size option

--- test/chardet.js
+++ test/chardet.js
@@ -20,11 +20,23 @@ describe('chardet', function() {
         done();
       });
     });
+
+    it('should detect encoding with smaller sample size', function(done) {
+      chardet.detectFile(path, { sampleSize: 32 }, function(err, res) {
+        assert.equal(err, null);
+        assert.equal(res, 'UTF-8');
+        done();
+      });
+    });
   });
 
   describe('#detectFileSync', function() {
     it('should detect encoding', function() {
       assert.equal(chardet.detectFileSync(path), 'UTF-8');
+    });
+
+    it('should detect encoding with smaller sample size', function() {
+      assert.equal(chardet.detectFileSync(path, { sampleSize: 32 }), 'UTF-8');
     });
   });
 });
