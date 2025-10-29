3eac8f7157aa72c6f70e73cfe0c3f7d7353a9828
Daniel Lemire <daniel@lemire.me>
2025-10-29 08:28:08 -0400

    added test

--- src/encoding/ascii.test.ts
+++ src/encoding/ascii.test.ts
@@ -7,3 +7,11 @@ describe('ASCII', () => {
     ).toBe('ASCII');
   });
 });
+
+describe('ASCII', () => {
+  it('should return ASCII', () => {
+    expect(
+      chardet.detectFileSync(__dirname + '/../test/data/encodings/shortascii',  { sampleSize: 32 }),
+    ).toBe('ASCII');
+  });
+});
