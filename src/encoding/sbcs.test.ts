dd69494c32fa723f457059654cbff05fd5e5e860
Dmitry Shirokov <deadrunk@gmail.com>
2026-06-20 11:43:57 +1000

    Windows 874 (#124)
    * feat: windows-874
    
    * Development snapshot
    
    * Fix build issues

--- src/encoding/sbcs.test.ts
+++ src/encoding/sbcs.test.ts
@@ -1,38 +1,43 @@
 import * as chardet from '..';
+import path from 'path';
 import { describe, expect, it, test } from 'vitest';
 
 describe('Singlebyte Character Sets', () => {
-  const base = __dirname + '/../test/data/encodings';
+  const base = path.join(__dirname, '/../test/data/encodings');
+
+  const detect = (filename: string) => {
+    return chardet.detectFileSync(path.join(base, filename));
+  };
 
   it('should return ISO-8859-1 (English)', () => {
-    expect(chardet.detectFileSync(base + '/iso88591_en')).toBe('ISO-8859-1');
+    expect(detect('iso88591_en')).toBe('ISO-8859-1');
   });
 
   it('should return ISO-8859-2 (Czech)', () => {
-    expect(chardet.detectFileSync(base + '/iso88592_cs')).toBe('ISO-8859-2');
+    expect(detect('iso88592_cs')).toBe('ISO-8859-2');
   });
 
   test.todo('should return ISO-8859-3');
   test.todo('should return ISO-8859-4');
 
   it('should return ISO-8859-5 (Russian)', () => {
-    expect(chardet.detectFileSync(base + '/iso88595_ru')).toBe('ISO-8859-5');
+    expect(detect('iso88595_ru')).toBe('ISO-8859-5');
   });
 
   it('should return ISO-8859-6 (Arabic)', () => {
-    expect(chardet.detectFileSync(base + '/iso88596_ar')).toBe('ISO-8859-6');
+    expect(detect('iso88596_ar')).toBe('ISO-8859-6');
   });
 
   it('should return ISO-8859-7 (Greek)', () => {
-    expect(chardet.detectFileSync(base + '/iso88597_el')).toBe('ISO-8859-7');
+    expect(detect('iso88597_el')).toBe('ISO-8859-7');
   });
 
   it('should return ISO-8859-8 (Hebrew)', () => {
-    expect(chardet.detectFileSync(base + '/iso88598_he')).toBe('ISO-8859-8');
+    expect(detect('iso88598_he')).toBe('ISO-8859-8');
   });
 
   it('should return ISO-8859-9 (Turkish)', () => {
-    expect(chardet.detectFileSync(base + '/iso88599_tr')).toBe('ISO-8859-9');
+    expect(detect('iso88599_tr')).toBe('ISO-8859-9');
   });
 
   test.todo('should return ISO-8859-10');
@@ -43,35 +48,39 @@ describe('Singlebyte Character Sets', () => {
   test.todo('should return ISO-8859-15');
   test.todo('should return ISO-8859-16');
 
+  it('should return windows-874 for Thai text', () => {
+    expect(detect('windows_874')).toBe('windows-874');
+  });
+
   it('should return windows-1250 (Czech)', () => {
-    expect(chardet.detectFileSync(base + '/windows_1250')).toBe('windows-1250');
+    expect(detect('windows_1250')).toBe('windows-1250');
   });
 
   it('should return windows-1251 (Russian)', () => {
-    expect(chardet.detectFileSync(base + '/windows_1251')).toBe('windows-1251');
+    expect(detect('windows_1251')).toBe('windows-1251');
   });
 
   it('should return windows-1252 (English)', () => {
-    expect(chardet.detectFileSync(base + '/windows_1252')).toBe('windows-1252');
+    expect(detect('windows_1252')).toBe('windows-1252');
   });
 
   it('should return windows-1253 (Greek)', () => {
-    expect(chardet.detectFileSync(base + '/windows_1253')).toBe('windows-1253');
+    expect(detect('windows_1253')).toBe('windows-1253');
   });
 
   it('should return windows-1254 (Turkish)', () => {
-    expect(chardet.detectFileSync(base + '/windows_1254')).toBe('windows-1254');
+    expect(detect('windows_1254')).toBe('windows-1254');
   });
 
   it('should return windows-1255 (Hebrew)', () => {
-    expect(chardet.detectFileSync(base + '/windows_1255')).toBe('windows-1255');
+    expect(detect('windows_1255')).toBe('windows-1255');
   });
 
   it('should return windows-1256 (Arabic)', () => {
-    expect(chardet.detectFileSync(base + '/windows_1256')).toBe('windows-1256');
+    expect(detect('windows_1256')).toBe('windows-1256');
   });
 
   it('should return KOI8-R (Russian)', () => {
-    expect(chardet.detectFileSync(base + '/koi8r')).toBe('KOI8-R');
+    expect(detect('koi8r')).toBe('KOI8-R');
   });
 });
