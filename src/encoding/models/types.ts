b74bec0af7e2323f87b972241e08fbe8f9f45a83
Dmitry Shirokov <deadrunk@gmail.com>
2026-07-12 10:46:20 +1000

    switch single byte matcher to new models from custom courpus

---
+++ src/encoding/models/types.ts
@@ -0,0 +1,18 @@
+import type { EncodingName } from '../../match';
+
+export interface HighByteDistribution {
+  readonly total: number;
+  readonly counts: readonly (readonly [number, number])[];
+}
+
+export interface GeneratedSBCSLanguage {
+  readonly language: string;
+  readonly ngrams: readonly number[];
+  readonly highBytes: HighByteDistribution;
+}
+
+export interface GeneratedSBCSModel {
+  readonly encoding: EncodingName;
+  readonly byteMap: readonly number[];
+  readonly languages: readonly GeneratedSBCSLanguage[];
+}
